10079448510f7c546bfeb8b9fb6c71ab760d5d2b
santigr17 <santigr17@gmail.com>
2019-09-12 05:28:19 -0600

    added bus

--- gui.py
+++ gui.py
@@ -1,7 +1,12 @@
 from tkinter import *
 from tkinter import ttk
-import core
 import time
+
+
+import core
+import connexion
+import memory
+
 
 class core_gui:
     def __init__(self, parent, core, color, position):
@@ -41,7 +46,7 @@ class core_gui:
         L_instr = Label(self.Cprocessor, font=('Agency FB',8), text = "Current Instruction:", bg = "white", borderwidth=1, relief="sunken")
         
 
-        L_id.place(x=70, y=8, width = 50)    
+        L_id.place(x=75, y=8, width = 50)    
         x,y = 15,28
         ancho = 75
         ancho2 = 40
@@ -63,9 +68,9 @@ class core_gui:
         x,y = 132, 28
         L_instr.place(x=x,y=y,width=110)
         y+=16
-        x2=x+110-ancho2
+        x2=x+110-ancho2-5
         temp = Entry(self.Cprocessor, font=('Agency FB',8), textvariable=instr, state='readonly')
-        temp.place(x=x2, y=y, width = ancho2)
+        temp.place(x=x2, y=y, width = ancho2+5)
         y+=16
         btn_log = Button(self.Cprocessor,text="LOG", command = self.log, bg = self.color, fg = 'light grey', font=('Agency FB',8))
         btn_log.place(x=x+10,y=y)
@@ -116,9 +121,38 @@ class core_gui:
 
             y+=15
             
+class bus_gui:
+    def __init__(self, parent, bus, position):
+        self.Cbus = Canvas(parent, bg ='light yellow', width = 768, height = 25) #width = 1024 * 3/4
+        x,y=position
+        self.Cbus.place(x=x, y=y)
+        self.dir = StringVar(value = bus.dir)
+        self.data = StringVar(value = bus.data)
+        
+        L_dir = Label(self.Cbus, font=('Agency FB',10), text = "Current DIR on the bus:", bg='white', borderwidth=1, relief="sunken")
+        L_dir.place(x=100,y=2, width=150)
+        
+        temp = Entry(self.Cbus, font=('Agency FB',10), textvariable=self.dir, state='readonly')
+        temp.place(x= 255, y=2, width=60)
+        
+        L_data = Label(self.Cbus, font=('Agency FB',10), text = "Current DATA on the bus:", bg='white', borderwidth=1, relief="sunken")
+        L_data.place(x=350,y=2, width=150)
+        
+        temp = Entry(self.Cbus, font=('Agency FB',10), textvariable=self.dir, state='readonly')
+        temp.place(x= 505, y=2, width=60)
+
+class memory_gui:
+    def __init__(self, parent, memory, position ):
+        pass
+
+
+
+
+
+mem = memory.memory()
+dataBus = connexion.bus(mem)
 
 root = Tk()
-
 
 root.title('Arquitectura de Computadores 2, Proyecto Programado 1')
 root.minsize(1024,600)
@@ -126,10 +160,11 @@ root.resizable(width=NO,height=NO)
 
 C_root = Canvas(root,bg='white')
 C_root.pack(fill=BOTH, expand=1)
-x=4
-new = core_gui(root, None, 'blue', (x,75))
-new = core_gui(root, None, 'green', (x+255,75))
-new = core_gui(root, None, 'cyan', (x+510,75))
-new = core_gui(root, None, 'salmon', (x+765,75))
+x, y = 4,300
 
+new = core_gui(root, None, 'blue', (x,y))
+new = core_gui(root, None, 'green', (x+255,y))
+new = core_gui(root, None, 'cyan', (x+510,y))
+new = core_gui(root, None, 'salmon', (x+765,y))
+bus = bus_gui(root, dataBus, (x+128,y - 40))
 root.mainloop()
